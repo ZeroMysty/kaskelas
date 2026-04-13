774dbbce4afefc7f16f91a42415797776913f6bf
Zaid Hifdzul Mumtaz <zaidmumtaz68@gmail.com>
2026-04-13 08:40:16 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,3 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
-
-dependencies {
-   implementation("androidx.core:core-splashscreen:1.0.1")
-}
