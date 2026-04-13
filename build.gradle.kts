9e92a774b183a29519f3db783b7a355044a74794
Zaid Hifdzul Mumtaz <zaidmumtaz68@gmail.com>
2026-04-13 08:37:02 +0700

    Update build.gradle.kts
    coba coba

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+
+dependencies {
+   implementation("androidx.core:core-splashscreen:1.0.1")
+}
